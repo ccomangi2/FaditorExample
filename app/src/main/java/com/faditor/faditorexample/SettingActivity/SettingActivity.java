486f95429f1be0469c427ca6a48fea99a3b6a3fb
ccomangi2 <ccomangi2@gmail.com>
2020-09-19 01:08:14 +0900

    설정 화면 구현

--- app/src/main/java/com/faditor/faditorexample/SettingActivity/SettingActivity.java
+++ app/src/main/java/com/faditor/faditorexample/SettingActivity/SettingActivity.java
@@ -1,29 +1,43 @@
 package com.faditor.faditorexample.SettingActivity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.faditor.faditorexample.MainActivity.MainActivity;
+import com.faditor.faditorexample.ProfileEditActivity.ProfileEditActivity;
 import com.faditor.faditorexample.R;
+
 
 public class SettingActivity extends AppCompatActivity {
     ImageButton back;
+    Button setProfile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
+        // 뒤로 가기
         back = (ImageButton)findViewById(R.id.back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // 프로필 수정 페이지 이동
+        setProfile = (Button)findViewById(R.id.setProfile);
+        setProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), ProfileEditActivity.class);
                 startActivity(intent);
             }
         });
