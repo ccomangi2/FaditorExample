163ffff5ff2bffd6481360dede5dc6f7e9624bab
ccomangi2 <ccomangi2@gmail.com>
2020-11-20 11:07:49 +0900

    11.20 로그인화면 레지스터화면 수정

---
+++ app/src/main/java/com/faditor/faditorexample/RegisterActivity/RegisterActivity.java
@@ -0,0 +1,17 @@
+package com.faditor.faditorexample.RegisterActivity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.faditor.faditorexample.R;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    }
+}
