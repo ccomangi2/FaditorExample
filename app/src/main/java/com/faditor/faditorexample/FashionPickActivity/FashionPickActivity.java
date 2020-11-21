46a22fdf165375224dc2651c48b03920ed083e23
ccomangi2 <ccomangi2@gmail.com>
2020-11-21 19:05:25 +0900

    11.21 관심 패션 선택 프로토타입 구현

--- app/src/main/java/com/faditor/faditorexample/FashionPickActivity/FashionPickActivity.java
+++ app/src/main/java/com/faditor/faditorexample/FashionPickActivity/FashionPickActivity.java
@@ -5,9 +5,10 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.faditor.faditorexample.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FashionPickActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
